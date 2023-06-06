6abf6e7ee448fa7453c0000d0ca1c9708d920610
Eunjin Cho <whdmswls9207@gmail.com>
2023-06-07 00:42:30 +0900

    finish ending
    엔딩 구현 끝

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -10,35 +10,64 @@ public class Dialogue : MonoBehaviour
 
     public Text EndingText;
     public int EndingIdx;
-    public string[] texts;
+    private string[] texts;
     private int clickCount;
     private int clickCount2 = 0;
-
+    public GameManager gameManager;
+    public GameObject[] Endings;
     // Start is called before the first frame update
     void Start()
     {
+
+        if (gameManager.totalKey < 3)
+            EndingIdx = 2;
+        else if(gameManager.totalKey < 10)
+            EndingIdx = 1;
+        else
+            EndingIdx = 0;
+
+        Endings[EndingIdx].SetActive(true);
+
         if (EndingIdx == 0)
         {
+            texts = new string[5];
             texts[0] = "이로써 은이의 모험이 막을 내렸다.";
             texts[1] = "보물 사냥꾼 은이는 세상의 모든 귀한 보물을 다 얻었다.";
-            texts[2] = "- Good Ending -";
+            texts[2] = "은이는 세계 최고의 보물 사냥꾼으로써 부자가 되어 궁전같은 집에서 돈을 펑펑 쓰며 행복한 노후를 보내게 되었다.";
+            texts[3] = "그러나 한번 사냥꾼은 영원한 사냥꾼! \n 은이는 새로운 보물을 얻기 위해, 다시 여행을 떠날 것이다.";
+
+            texts[4] = "- Good Ending -";
         }
 
         if (EndingIdx == 1)
         {
+            texts = new string[6];
             texts[0] = "이로써 은이의 모험이 막을 내렸다.";
             texts[1] = "보물 사냥꾼 은이는 세상의 모든 귀한 보물을 다 얻지는 못했다.";
             texts[2] = "그러나 은이는 보물들을 팔아 돈을 많이 벌었고, 만족하기로 했다.";
             texts[3] = "은이는 그 돈으로 귀농을 시작했고, 보물 사냥꾼이 아닌 농부로써 두번째 삶을 살게 되었다.";
-            texts[4] = "- Normal Ending -";
+
+            texts[4] = "가끔 잘나가는 보물 사냥꾼이었던 과거가 생각났지만," +
+                "농부로써의 삶도 즐겁고 행복하다.";
+
+            texts[5] = "- Normal Ending -";
         }
 
         if (EndingIdx == 2)
         {
+            texts = new string[10];
             texts[0] = "이로써 은이의 모험이 막을 내렸다.";
             texts[1] = "보물 상자를 열어서 얻은 것은 작고 초라한 보석 하나였다..";
-            texts[2] = "비통하다.... 열쇠를 좀 더 많이 모아볼 걸 그랬다.";
-            texts[3] = "- Bad Ending -";
+
+            texts[2] = "그마저도 가짜 보석이었기에, 은이는 아무런 돈도 벌지 못했다.";
+            texts[3] = "게다가 가짜 보석을 팔려 한 죄로, 마을에서 추방당하기까지 했다.";
+            texts[4] = "은이는 숲속에서 누군가의 해골과 밤을 보내야했다.";
+            texts[5] = "배가 고프지만 돈이 없어 독버섯인지 아닌지도 모르는 버섯을 주워 불에 구워먹어야한다.";
+            texts[6] = "은이의 삶은 어쩌다 이렇게 되었을까?";
+            texts[7] = "나도 한때는 잘나가는 보물 사냥꾼이었는데...!!";
+
+            texts[8] = "비통하다.... 열쇠를 좀 더 많이 모아볼 걸 그랬다.";
+            texts[9] = "- Bad Ending -";
         }
 
         StartCoroutine(Typing(texts[clickCount]));
@@ -53,7 +82,6 @@ public class Dialogue : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) && clickCount < texts.Length)
         {
-            //Debug.Log("클릭");
             if (clickCount2 == 0)
             {
                 StartCoroutine(Typing(texts[clickCount]));
@@ -65,6 +93,9 @@ public class Dialogue : MonoBehaviour
                 clickCount2=2;
             }
 
+        }else if(Input.GetMouseButtonDown(0))
+        {
+            gameManager.clear();
         }
     }
 
